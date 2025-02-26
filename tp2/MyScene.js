47358c957916f5a01db1942a015a838f09a977e0
Lucas Faria <lucasfaria@outlook.pt>
2025-02-26 16:16:12 +0000

    Added the unitCubeQuad (ex4)

--- tp2/MyScene.js
+++ tp2/MyScene.js
@@ -1,6 +1,7 @@
 import { CGFscene, CGFcamera, CGFaxis } from "../lib/CGF.js";
 import { MyTangram } from "./MyTangram.js";
 import { MyUnitCube } from "./MyUnitCube.js";
+import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
 
 /**
  * MyScene
@@ -28,6 +29,7 @@ export class MyScene extends CGFscene {
     this.axis = new CGFaxis(this);
     this.tangram = new MyTangram(this);
     this.unitCube = new MyUnitCube(this);
+    this.quadCube =  new MyUnitCubeQuad(this);
 
     //Objects connected to MyInterface
     this.displayAxis = true;
@@ -110,8 +112,11 @@ export class MyScene extends CGFscene {
     this.pushMatrix();
     this.translate(0,0,-0.5);
     this.scale(4,4,0.5);
-    this.unitCube.display();
+    this.quadCube.display();
+    //this.unitCube.display();
     this.popMatrix();
+
+    
 
 
     // ---- END Primitive drawing section
