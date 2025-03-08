bfd6ee19897863b6cfa09ba7c62895a4747301a6
Lucas Faria <lucasfaria@outlook.pt>
2025-03-08 15:48:46 +0000

    Added a prism with one stack. Still need to improve the normals

--- tp3/MyScene.js
+++ tp3/MyScene.js
@@ -5,6 +5,7 @@ import { MyPlane } from "./MyPlane.js";
 import {MyTangram} from "./MyTangram.js"
 import { MyUnitCubeQuad } from "./MyUnitCubeQuad.js";
 import {MyUnitCube} from "./MyUnitCube.js";
+import { MyPrism } from "./MyPrism.js";
 
 /**
 * MyScene
@@ -35,11 +36,12 @@ export class MyScene extends CGFscene {
         this.pyramid = new MyPyramid(this, 3, 1);
         this.tangram = new MyTangram(this);
         this.cube = new MyUnitCube(this);
+        this.prism = new MyPrism(this, 6, 1);
         
-        this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.cube];
+        this.objects = [this.plane, this.pyramid, this.cone, this.tangram, this.cube, this.prism];
 
         // Labels and ID's for object selection on MyInterface
-        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram' : 3, 'Cube': 4};
+        this.objectIDs = { 'Plane': 0 , 'Pyramid': 1, 'Cone': 2, 'Tangram' : 3, 'Cube': 4, 'Prism': 5};
 
         //Other variables connected to MyInterface
         this.selectedObject = 0;
@@ -191,6 +193,7 @@ export class MyScene extends CGFscene {
             this.objects[this.selectedObject].disableNormalViz();
         
         this.objects[this.selectedObject].display();
+        
         this.popMatrix();
         // ---- END Primitive drawing section
     }
