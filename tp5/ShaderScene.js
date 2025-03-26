f759cf30d7c3ab56f71d0d74e730b0471e3696b5
Lucas Faria <lucasfaria@outlook.pt>
2025-03-26 15:08:52 +0000

    Finnished the second part of TP5

--- tp5/ShaderScene.js
+++ tp5/ShaderScene.js
@@ -71,11 +71,11 @@ export class ShaderScene extends CGFscene {
 		this.appearance.setSpecular(0.0, 0.0, 0.0, 1);
 		this.appearance.setShininess(120);
 
-		this.texture = new CGFtexture(this, "textures/texture.jpg");
+		this.texture = new CGFtexture(this, "textures/waterMap.jpg");
 		this.appearance.setTexture(this.texture);
 		this.appearance.setTextureWrap('REPEAT', 'REPEAT');
 
-		this.texture2 = new CGFtexture(this, "textures/FEUP.jpg");
+		this.texture2 = new CGFtexture(this, "textures/waterTex.jpg");
 
 		// shaders initialization
 
@@ -91,15 +91,20 @@ export class ShaderScene extends CGFscene {
 			new CGFshader(this.gl, "shaders/texture1.vert", "shaders/convolution.frag"),
 			new CGFshader(this.gl, "shaders/shader1.vert", "shaders/shader1.frag"),
 			new CGFshader(this.gl, "shaders/shader1anim.vert", "shaders/shader1anim.frag"),
-			new CGFshader(this.gl, "shaders/shader1.vert", "shaders/shader1sepia.frag")
+			new CGFshader(this.gl, "shaders/shader1.vert", "shaders/shader1sepia.frag"),
+			new CGFshader(this.gl, "shaders/water.vert", "shaders/water.frag")
 		];
 
 		// additional texture will have to be bound to texture unit 1 later, when using the shader, with "this.texture2.bind(1);"
 		this.testShaders[4].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[5].setUniformsValues({ uSampler2: 1 });
 		this.testShaders[6].setUniformsValues({ uSampler2: 1 });
+		this.testShaders[6].setUniformsValues({ uSampler: 0 });
 		this.testShaders[6].setUniformsValues({ timeFactor: 0 });
 		this.testShaders[10].setUniformsValues({ timeFactor: 0 });
+		this.testShaders[12].setUniformsValues({ uSampler2: 1 });
+		this.testShaders[12].setUniformsValues({ uSampler: 0 });
+		this.testShaders[12].setUniformsValues({ timeFactor: 0 });
 
 
 		// Shaders interface variables
@@ -114,9 +119,10 @@ export class ShaderScene extends CGFscene {
 			'Animation example': 6,
 			'Sepia': 7,
 			'Convolution': 8,
-			'Shader Custom 1': 9,
+			'Shader Custom': 9,
 			'Shader Custom Anim' : 10,
-			"Shader Custom Sepia": 11
+			"Shader Custom Sepia": 11,
+			"Water Shader" : 12
 		};
 
 		// shader code panels references
@@ -202,7 +208,8 @@ export class ShaderScene extends CGFscene {
 			// Doing the modulus (%) by 100 makes the timeFactor loop between 0 and 99
 			// ( so the loop period of timeFactor is 100 times 100 ms = 10s ; the actual animation loop depends on how timeFactor is used in the shader )
 			this.testShaders[6].setUniformsValues({ timeFactor: t / 100 % 100 });
-			this.testShaders[10].setUniformsValues({ timeFactor: t / 100 % 100 });
+		this.testShaders[10].setUniformsValues({ timeFactor: t / 100 % 100 });
+		this.testShaders[12].setUniformsValues({ timeFactor: t / 100 % 100 });
 	}
 
 	// main display function
---
+++ tp5/shaders/water.vert
@@ -0,0 +1,22 @@
+attribute vec3 aVertexPosition;
+attribute vec3 aVertexNormal;
+attribute vec2 aTextureCoord;
+
+uniform mat4 uMVMatrix;
+uniform mat4 uPMatrix;
+uniform mat4 uNMatrix;
+uniform float timeFactor;
+
+varying vec2 vTextureCoord;
+
+uniform sampler2D uSampler;
+
+void main() {
+    vTextureCoord = aTextureCoord;
+    vec3 offset=vec3(0.0,0.0,1.0);
+    offset = offset * texture2D(uSampler, vTextureCoord).b * 0.1;
+	gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition + offset, 1.0);
+
+	
+}
+
